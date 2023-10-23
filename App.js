26b8d46a274381e9f5be0985675c88c9ec69535a
Erin Yan <changyannnnnn@gmail.com>
2023-10-22 19:41:06 -0700

    added back splash screen

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { enableScreens } from 'react-native-screens';
 import SignUpScreen from './screens/Authentication/SignUpScreen';
 import LoginScreen from './screens/Authentication/LoginScreen';
+import SplashScreen from './screens/SplashScreen';
 
 enableScreens();
-
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [isSplash, setIsSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsSplash(false);
+    }, 2000); // 2 seconds delay
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isSplash) {
+    return <SplashScreen />;
+  }
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
