f1c469de83850cbd480a9b16d2dbcd255d16e6a9
Erin Yan <changyannnnnn@gmail.com>
2023-10-25 01:50:22 -0700

    added reset password screen

---
+++ screens/Authentication/ResetPasswordScreen.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default function ResetPasswordScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+
+  const handleResetPassword = async () => {
+    // Placeholder logic for resetting password
+    console.log('Handle reset password logic here');
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TouchableOpacity onPress={handleResetPassword} style={styles.button}>
+        <Text style={styles.buttonText}>Reset Password</Text>
+      </TouchableOpacity>
+      <Text style={styles.footer}>
+        Remember your password? &nbsp;
+        <Text style={styles.footerLink} onPress={() => navigation.navigate('Login')}>
+          Login
+        </Text>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  input: {
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    padding: 8,
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  footer: {
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  footerLink: {
+    color: '#007BFF',
+  },
+});
