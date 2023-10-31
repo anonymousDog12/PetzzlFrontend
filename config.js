bf8c7f53cd6a85621b8afa93ce484be7b6c97672
Erin Yan <changyannnnnn@gmail.com>
2023-10-31 02:54:21 -0700

    updated config to react native config

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
-export const ENV = process.env.REACT_APP_ENV;
+import Config from 'react-native-config';
+
+export const ENV = Config.REACT_APP_ENV;
 
 export const CONFIG = {
   DEV: {
-    BACKEND_URL: process.env.REACT_APP_BACKEND_DEV,
+    BACKEND_URL: Config.REACT_APP_BACKEND_DEV,
   },
   PROD: {
-    BACKEND_URL: process.env.REACT_APP_BACKEND_PROD,
+    BACKEND_URL: Config.REACT_APP_BACKEND_PROD,
   },
 }[ENV];
