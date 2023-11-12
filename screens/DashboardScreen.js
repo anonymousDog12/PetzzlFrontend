1dc7a2f40f1f5ae246f1ad27b10e0253b198162b
Erin Yan <changyannnnnn@gmail.com>
2023-11-11 18:58:58 -0800

    successfully added pizza icon

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,10 +1,20 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { useSelector } from "react-redux"; // Import useSelector
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const DashboardScreen = () => {
-  // Get the user data from Redux state
   const user = useSelector(state => state.auth.user);
+  const navigation = useNavigation();
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Icon name="pizza-outline" size={30} color="#900" solid />
+      ),
+    });
+  }, [navigation]);
 
   return (
     <View style={styles.container}>
@@ -17,8 +27,8 @@ const DashboardScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
