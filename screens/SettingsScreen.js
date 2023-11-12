f87deba0ab913de3671bcb3d61f3dea3836bcf59
Erin Yan <changyannnnnn@gmail.com>
2023-11-12 13:49:44 -0800

    initialized log out option

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { useSelector } from "react-redux"; // Import useSelector
+import { StyleSheet, Text, View, Button } from "react-native"; // Import Button component
+import { useSelector } from "react-redux";
 
 const SettingsScreen = () => {
   // Get the user data from Redux state
   const user = useSelector(state => state.auth.user);
 
+  // Function to handle log out
+  const handleLogout = () => {
+    console.log("logging out");
+    // Here you will later add the logic for actual logout
+  };
+
   return (
     <View style={styles.container}>
       <Text>Settings</Text>
+      {/* Add a Button for logging out */}
+      <Button
+        title="Log Out"
+        onPress={handleLogout} // Set the onPress to the handleLogout function
+      />
     </View>
   );
 };
@@ -18,6 +29,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 20, // Add some padding for aesthetics
   },
 });
 
