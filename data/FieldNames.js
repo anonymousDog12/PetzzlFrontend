a1d7987fc7bb57f0571688b24b481b70ec856590
Erin Yan <changyannnnnn@gmail.com>
2023-11-12 19:46:18 -0800

    added pet type to default profile pic mapping

--- data/FieldNames.js
+++ data/FieldNames.js
@@ -19,3 +19,14 @@ export const PET_TYPES = {
   TURTLE: 'turtle',
   OTHER: 'other',
 }
+
+export const DEFAULT_PROFILE_PICS = {
+  [PET_TYPES.DOG]: 'https://petzzl.sfo3.cdn.digitaloceanspaces.com/default-profile-pics/dog.png',
+  [PET_TYPES.CAT]: 'https://petzzl.sfo3.cdn.digitaloceanspaces.com/default-profile-pics/cat.png',
+  [PET_TYPES.BIRD]: 'https://petzzl.sfo3.cdn.digitaloceanspaces.com/default-profile-pics/bird.png',
+  [PET_TYPES.FISH]: 'https://petzzl.sfo3.cdn.digitaloceanspaces.com/default-profile-pics/fish.png',
+  [PET_TYPES.HORSE]: 'https://petzzl.sfo3.cdn.digitaloceanspaces.com/default-profile-pics/horse.png',
+  [PET_TYPES.RABBIT]: 'https://petzzl.sfo3.cdn.digitaloceanspaces.com/default-profile-pics/rabbit.png',
+  [PET_TYPES.TURTLE]: 'https://petzzl.sfo3.cdn.digitaloceanspaces.com/default-profile-pics/turtle.png',
+  [PET_TYPES.OTHER]: 'https://petzzl.sfo3.cdn.digitaloceanspaces.com/default-profile-pics/other.png',
+};
