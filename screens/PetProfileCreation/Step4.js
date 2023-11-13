b8fe390df18b29306d0adc151e7f1b1aa8c3b9a1
Erin Yan <changyannnnnn@gmail.com>
2023-11-12 22:06:15 -0800

    added redux thunk to resolve redirection error

--- screens/PetProfileCreation/Step4.js
+++ screens/PetProfileCreation/Step4.js
@@ -1,38 +1,27 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
-import { CommonActions } from '@react-navigation/native';
 import { useDispatch } from "react-redux";
 import { usePetProfile } from "../../contexts/PetProfileContext";
 import { PET_PAGE_CREATION_FIELD_NAMES } from "../../data/FieldNames";
-import { setHasPets, setNewPetProfile } from "../../redux/actions/petProfile";
+import { setHasPetsAndNavigate } from "../../redux/actions/petProfile";
 
-const Step4 = ({ navigation, route }) => {
+const Step4 = ({ navigation }) => {
   const dispatch = useDispatch();
-
   const { petProfile } = usePetProfile();
 
-  const navigateToDashboard = () => {
-    // First, update the Redux state to indicate that the user has pets and
-    // a new pet profile has been created
-    dispatch(setHasPets(true));
-    dispatch(setNewPetProfile(false));
-
-    // Use a timeout to allow the state update to propagate
-    setTimeout(() => {
-      // Now perform the navigation
-      navigation.reset({
-        index: 0,
-        routes: [
-          {
-            name: 'BottomNavBar',
-            params: {
-              screen: 'Dashboard', // Specify the screen within the BottomNavBar
-            },
-          },
-        ],
+  const continueToDashboard = () => {
+    dispatch(setHasPetsAndNavigate(true, navigation))
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'BottomNavBar', params: { screen: 'Dashboard' } }],
+        });
+      })
+      .catch(error => {
+        console.error('Navigation error:', error);
       });
-    }, 0); // Timeout with 0 to push the action to the end of the event queue
   };
+
 
 
   return (
@@ -40,7 +29,7 @@ const Step4 = ({ navigation, route }) => {
       <Text>Welcome, {petProfile[PET_PAGE_CREATION_FIELD_NAMES.PET_NAME]}!</Text>
       <Button
         title="Continue"
-        onPress={navigateToDashboard}
+        onPress={continueToDashboard}
       />
     </View>
   );
