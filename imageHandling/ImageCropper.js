9267d839772f2f1d9657c346394bc1cfd6e86d6a
Erin Yan <changyannnnnn@gmail.com>
2023-11-15 20:13:56 +0000

    Pa 93 support take photo as profile pic

--- imageHandling/ImageCropper.js
+++ imageHandling/ImageCropper.js
@@ -1,14 +1,15 @@
-import ImageCropPicker from 'react-native-image-crop-picker';
-import SecureStorage from 'react-native-secure-storage';
-import axios from 'axios';
+import axios from "axios";
+import ImageCropPicker from "react-native-image-crop-picker";
+import SecureStorage from "react-native-secure-storage";
 import { CONFIG } from "../config";
+
 
 const ImageCropper = {
   openCropper: async (uri, petId, onSuccess) => {
     try {
-      const accessToken = await SecureStorage.getItem('access');
-      console.log('JWT Token:', accessToken);
-      console.log('Pet ID:', petId);
+      const accessToken = await SecureStorage.getItem("access");
+      console.log("JWT Token:", accessToken);
+      console.log("Pet ID:", petId);
 
       const croppedImage = await ImageCropPicker.openCropper({
         path: uri,
@@ -19,32 +20,32 @@ const ImageCropper = {
         freeStyleCropEnabled: true,
       });
 
-      console.log('Cropped image:', croppedImage);
+      console.log("Cropped image:", croppedImage);
 
       // Prepare the form data to upload
       const formData = new FormData();
-      formData.append('file', {
+      formData.append("file", {
         uri: croppedImage.path,
         type: croppedImage.mime,
-        name: croppedImage.path.split('/').pop(),
+        name: croppedImage.path.split("/").pop(),
       });
-      formData.append('pet_id', petId);
+      formData.append("pet_id", petId);
 
       // Perform the image upload
       const uploadResponse = await axios.post(`${CONFIG.BACKEND_URL}/api/petprofiles/upload_pet_profile_pic/`, formData, {
         headers: {
-          'Authorization': `JWT ${accessToken}`,
-          'Content-Type': 'multipart/form-data',
+          "Authorization": `JWT ${accessToken}`,
+          "Content-Type": "multipart/form-data",
         },
       });
 
-      console.log('Upload response:', uploadResponse.data);
+      console.log("Upload response:", uploadResponse.data);
 
       if (uploadResponse.status === 200) {
         onSuccess();
       }
     } catch (e) {
-      console.error('Error during image cropping or upload:', e);
+      console.error("Error during image cropping or upload:", e);
     }
   },
 };
