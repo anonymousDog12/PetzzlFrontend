058d95c3169148c810123786b90b5416c9cd4e37
Erin Yan <changyannnnnn@gmail.com>
2023-12-02 17:08:46 +0000

    Pa 140 enhance ui of post detail page

--- screens/PostDetailScreen.js
+++ screens/PostDetailScreen.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
+import { View, Text, Image, Dimensions, StyleSheet, SafeAreaView } from "react-native";
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
 import { CONFIG } from "../config";
 
+const { width } = Dimensions.get('window');
+const imageContainerHeight = 300;
+
 const PostDetailScreen = ({ route }) => {
-  const { postId } = route.params;
   const [postDetails, setPostDetails] = useState(null);
+  const { postId, petProfile } = route.params;
+
 
   useEffect(() => {
-    // Fetch full-size media for the post
     const fetchPostDetails = async () => {
       try {
         const response = await fetch(`${CONFIG.BACKEND_URL}/api/mediaposts/post_media/${postId}/full/`);
@@ -21,21 +25,77 @@ const PostDetailScreen = ({ route }) => {
   }, [postId]);
 
   if (!postDetails) {
-    return <Text>Loading...</Text>; // Or any other loading indicator
+    return <Text>Loading...</Text>;
   }
 
   return (
-    <ScrollView>
-      <Text>{postDetails.caption}</Text>
-      {postDetails.media.map((mediaItem) => (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.profileHeader}>
         <Image
-          key={mediaItem.media_id}
-          source={{ uri: mediaItem.full_size_url }}
-          style={{ width: '100%', height: 300 }} // Adjust styling as needed
+          source={{ uri: petProfile.profile_pic_thumbnail_small }}
+          style={styles.profilePic}
         />
-      ))}
-    </ScrollView>
+        <Text style={styles.username}>{petProfile.pet_name}</Text>
+      </View>
+      <View style={{ height: imageContainerHeight }}>
+        <SwiperFlatList
+          index={0}
+          showPagination
+          paginationStyle={styles.paginationStyle}
+          paginationStyleItem={styles.paginationStyleItem}
+          data={postDetails.media}
+          renderItem={({ item }) => (
+            <View style={{ width, height: imageContainerHeight }}>
+              <Image
+                source={{ uri: item.full_size_url }}
+                style={styles.imageStyle}
+                resizeMode='contain' // This will keep the aspect ratio
+                onError={(e) => console.log('Failed to load image', e.nativeEvent.error)}
+              />
+            </View>
+          )}
+        />
+      </View>
+      <Text style={{ textAlign: 'center', padding: 10 }}>{postDetails.caption}</Text>
+
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  paginationStyle: {
+    position: 'absolute',
+    bottom: 15, // Adjust this value to place the dots just above the bottom of the image container
+    alignSelf: 'center',
+  },
+  imageStyle: {
+    width: '100%',
+    height: '100%',
+  },
+  paginationStyleItem: {
+    width: 8, // Set the width of the dots
+    height: 8, // Set the height of the dots
+    borderRadius: 4, // Half of either width or height will ensure a circular shape
+  },
+  profileHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: 10,
+    backgroundColor: 'white', // or any color that suits your app design
+    borderBottomWidth: 1, // optional, for a subtle separation from the content below
+    borderBottomColor: '#e1e1e1', // optional, color for the border line
+  },
+  profilePic: {
+    width: 50, // adjust the size as needed
+    height: 50, // adjust the size as needed
+    borderRadius: 25, // half the size to make it round
+    marginRight: 10,
+  },
+  username: {
+    fontWeight: 'bold',
+    // add more styles if needed
+  },
+});
 
 export default PostDetailScreen;
