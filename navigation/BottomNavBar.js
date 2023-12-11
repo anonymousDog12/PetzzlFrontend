8a0665e5f84aee16d88fe4491c6a873132c62235
Erin Yan <changyannnnnn@gmail.com>
2023-12-11 02:17:33 +0000

    properly showed bottom nav bar

--- navigation/BottomNavBar.js
+++ navigation/BottomNavBar.js
@@ -3,6 +3,9 @@ import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import DashboardScreen from "../screens/DashboardScreen";
 import FeedScreen from "../screens/FeedScreen";
+import LikerListScreen from "../screens/LikerListScreen";
+import PostDetailScreen from "../screens/PostDetailScreen";
+
 
 const Tab = createBottomTabNavigator();
 
@@ -11,27 +14,22 @@ const EmptyScreen = () => null;
 const BottomNavBar = ({ initialRouteName }) => {
   const navigation = useNavigation();
 
-
   return (
-    <Tab.Navigator
-      initialRouteName={initialRouteName}
-      screenOptions={({ route }) => ({
-        // Remove the tabBarOnPress option as it's no longer needed here.
-      })}
-    >
+    <Tab.Navigator initialRouteName={initialRouteName}>
       <Tab.Screen name="Feed" component={FeedScreen} />
       <Tab.Screen
         name="NewPost"
-        component={EmptyScreen}  // This screen is just a trigger for the modal.
+        component={EmptyScreen} // This screen is just a trigger for the modal.
         listeners={{
           tabPress: (e) => {
-            // Prevent default tab press behavior and navigate to the modal screen.
             e.preventDefault();
-            navigation.navigate('NewPostModal');
+            navigation.navigate("NewPostModal");
           },
         }}
       />
       <Tab.Screen name="Dashboard" component={DashboardScreen} />
+      <Tab.Screen name="PostDetailScreen" component={PostDetailScreen} options={{ tabBarButton: () => null }} />
+      <Tab.Screen name="LikerListScreen" component={LikerListScreen} options={{ tabBarButton: () => null }} />
     </Tab.Navigator>
   );
 };
