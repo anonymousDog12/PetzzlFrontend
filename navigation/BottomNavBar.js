641d82de44d06120ebf5c84662d4b0491773a5d7
Erin Yan <changyannnnnn@gmail.com>
2023-12-17 17:28:22 -0800

    enhanced bottom nav bar icons

--- navigation/BottomNavBar.js
+++ navigation/BottomNavBar.js
@@ -3,7 +3,7 @@ import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import DashboardScreen from "../screens/DashboardScreen";
 import FeedScreen from "../screens/FeedScreen";
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,11 +13,32 @@ const BottomNavBar = ({ initialRouteName }) => {
   const navigation = useNavigation();
 
   return (
-    <Tab.Navigator initialRouteName={initialRouteName}>
+    <Tab.Navigator
+      initialRouteName={initialRouteName}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Feed') {
+            iconName = focused ? 'home' : 'home-outline'; // Updated icon names
+          } else if (route.name === 'NewPost') {
+            iconName = focused ? 'add-circle' : 'add-circle-outline';
+          } else if (route.name === 'Dashboard') {
+            iconName = focused ? 'person' : 'person-outline'; // Updated icon names
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
       <Tab.Screen name="Feed" component={FeedScreen} />
       <Tab.Screen
         name="NewPost"
-        component={EmptyScreen} // This screen is just a trigger for the modal.
+        component={EmptyScreen}
         listeners={{
           tabPress: (e) => {
             e.preventDefault();
@@ -26,7 +47,6 @@ const BottomNavBar = ({ initialRouteName }) => {
         }}
       />
       <Tab.Screen name="Dashboard" component={DashboardScreen} />
-
     </Tab.Navigator>
   );
 };
