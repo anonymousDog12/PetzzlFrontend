5c6fa313ae848148d2a3e531365c2470d491cff6
Erin Yan <changyannnnnn@gmail.com>
2023-12-18 23:17:05 -0800

    adjusted sign up form styles

--- src/screens/Authentication/AuthenticationStyles.js
+++ src/screens/Authentication/AuthenticationStyles.js
@@ -5,28 +5,38 @@ export const authStyles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 20,
-    backgroundColor: '#ffc02c',
+    backgroundColor: '#f2f2f2', // Set to light grey background
   },
   input: {
-    borderBottomWidth: 1,
+    backgroundColor: '#ffffff', // White background color
+    borderWidth: 1, // Define border width
+    borderColor: '#cccccc', // Light grey border color
+    borderRadius: 50, // Rounded look
     marginBottom: 15,
-    padding: 8,
+    paddingHorizontal: 12, // Only horizontal padding affects width
+    paddingVertical: 12, // Padding top and bottom
+    fontSize: 16,
+    height: 50, // Set fixed height for inputs
   },
   button: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 5,
+    backgroundColor: '#ffc02c', // Set button background to the specified orange
+    borderRadius: 50, // Match border radius with inputs for consistency
+    marginVertical: 10, // Add vertical margin for spacing
+    height: 50, // Match the height with inputs
+    justifyContent: 'center', // Center the text vertically
   },
   buttonText: {
-    color: '#fff',
+    color: '#ffffff', // White text color for the button
     textAlign: 'center',
+    fontWeight: 'bold', // Make the text bold
+    fontSize: 18, // Increase font size for better readability
   },
   footer: {
     marginTop: 20,
     textAlign: 'center',
   },
   footerLink: {
-    color: '#007BFF',
+    color: '#007BFF', // Keep the link color as it is
   },
   error: {
     color: 'red',
--- src/screens/Authentication/SignUpScreen.js
+++ src/screens/Authentication/SignUpScreen.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
+import { KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity } from "react-native";
 import { useDispatch } from "react-redux";
 import { login, signup } from "../../redux/actions/auth";
 import { extractErrorMessages } from "../../utils/auth";
-import { authStyles } from './AuthenticationStyles';
+import { authStyles } from "./AuthenticationStyles";
 
 
 export default function SignUpScreen({ navigation }) {
@@ -36,7 +36,6 @@ export default function SignUpScreen({ navigation }) {
       } else {
         console.log("login in successful");
       }
-
     }
   };
 
@@ -102,37 +101,3 @@ export default function SignUpScreen({ navigation }) {
     </KeyboardAvoidingView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 20,
-  },
-  input: {
-    borderBottomWidth: 1,
-    marginBottom: 15,
-    padding: 8,
-  },
-  button: {
-    backgroundColor: "#007BFF",
-    padding: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: "#fff",
-    textAlign: "center",
-  },
-  footer: {
-    marginTop: 20,
-    textAlign: "center",
-  },
-  footerLink: {
-    color: "#007BFF",
-  },
-  error: {
-    color: "red",
-    textAlign: "center",
-    marginTop: 10,
-  },
-});
