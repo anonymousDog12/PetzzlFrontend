a479fce63e90b87c174174bd3fdb0f867caa5263
Erin Yan <changyannnnnn@gmail.com>
2023-12-20 19:15:26 -0800

    allowed space in pet name

--- src/screens/PetProfileCreation/Step1.js
+++ src/screens/PetProfileCreation/Step1.js
@@ -1,12 +1,11 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import React, { useLayoutEffect, useState } from "react";
-import { Button, KeyboardAvoidingView, Text, TextInput } from "react-native";
+import { Button, KeyboardAvoidingView, Text, TextInput, TouchableOpacity } from "react-native";
 import { useDispatch } from "react-redux";
 import { usePetProfile } from "../../contexts/PetProfileContext";
 import { PET_PAGE_CREATION_FIELD_NAMES } from "../../data/FieldNames";
 import { setNewPetProfile } from "../../redux/actions/petProfile";
 import PetProfileCreationStyles from "./PetProfileCreationStyles";
-import { TouchableOpacity } from 'react-native';
 
 
 const Step1 = () => {
@@ -30,20 +29,21 @@ const Step1 = () => {
     if (name.length < 2 || name.length > 50) {
       return "Pet name must be between 2 and 50 characters.";
     }
-    if (!/^[a-zA-Z0-9]+$/.test(name)) {
-      return "Pet name must be alphanumeric only.";
+    if (!/^[a-zA-Z0-9 ]+$/.test(name)) {
+      return "Pet name must be alphanumeric and may include spaces.";
     }
     return "";
   };
 
-
   const handleContinue = () => {
-    const validationError = validatePetName(petName);
+    const trimmedPetName = petName.trim().replace(/\s+/g, " ");
+
+    const validationError = validatePetName(trimmedPetName);
     if (validationError) {
       setError(validationError);
       return;
     }
-    updateProfile({ [PET_PAGE_CREATION_FIELD_NAMES.PET_NAME]: petName });
+    updateProfile({ [PET_PAGE_CREATION_FIELD_NAMES.PET_NAME]: trimmedPetName });
     navigation.navigate("PetProfileCreationStep2");
   };
 
